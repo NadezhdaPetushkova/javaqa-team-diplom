3d9fbe397844d2fc2bb43d68b79e7bcabaa27663
Petushkova Nadezhda <nadin202@mail.ru>
2023-06-18 01:29:00 +0300

    CreditAccount

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -6,8 +6,20 @@ import org.junit.jupiter.api.Test;
 public class CreditAccountTest {
 
     @Test
-    public void exceptionRateBelowNull () {
+    public void shouldAddToPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
 
+        account.add(3_000);
+
+        Assertions.assertEquals(3_000, account.getBalance());
+    }
+
+    @Test
+    public void exceptionRateBelowNull() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             CreditAccount account = new CreditAccount(
                     5_000,
@@ -18,20 +30,17 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void exceptionRateEqualNull () {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CreditAccount account = new CreditAccount(
-                    5_000,
-                    5_000,
-                    0
-            );
-        });
+    public void exceptionRateEqualNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                0
+        );
+        Assertions.assertEquals(0, account.getRate());
     }
 
-    @Test // Не реализовано исключение вида IllegalArgumentException для кредитного лимита (creditLimit - неотрицательное число)
-    public void exceptionCreditLimitBelowNull () {
-
+    @Test
+    public void exceptionCreditLimitBelowNull() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             CreditAccount account = new CreditAccount(
                     5_000,
@@ -41,21 +50,18 @@ public class CreditAccountTest {
         });
     }
 
-    @Test // Не реализовано исключение вида IllegalArgumentException для кредитного лимита (creditLimit - неотрицательное число) и соответсвенно не ноль
-    public void exceptionCreditLimitNull () {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CreditAccount account = new CreditAccount(
-                    5_000,
-                    0,
-                    15
-            );
-        });
+    @Test
+    public void exceptionCreditLimitNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                0,
+                15
+        );
+        Assertions.assertEquals(0, account.getCreditLimit());
     }
 
-    @Test // Не реализовано исключение вида IllegalArgumentException начального баланса (initialBalance - неотрицательное число)
-    public void exceptionInitialBalanceBelowNull () {
-
+    @Test
+    public void exceptionInitialBalanceBelowNull() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             CreditAccount account = new CreditAccount(
                     -1,
@@ -65,43 +71,17 @@ public class CreditAccountTest {
         });
     }
 
-    @Test // Не реализовано исключение вида IllegalArgumentException начального баланса (initialBalance - неотрицательное число) и соответсвенно не ноль
-    public void exceptionInitialBalanceNull () {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CreditAccount account = new CreditAccount(
-                    0,
-                    5_000,
-                    15
-            );
-        });
+    @Test
+    public void exceptionInitialBalanceNull() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+        Assertions.assertEquals(0, account.getBalance());
     }
 
-    @Test // Не реализовано исключение вида IllegalArgumentException для условия: при создании, баланс кредитного счёта изначально выставляется в кредитный лимит.
-    public void exceptionInitialBalanceMoreCreditLimit () {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CreditAccount account = new CreditAccount(
-                    10_000,
-                    5_000,
-                    15
-            );
-        });
-    }
-
-    @Test // Не реализовано исключение вида IllegalArgumentException для условия: при создании, баланс кредитного счёта изначально выставляется в кредитный лимит.
-    public void exceptionInitialBalanceLessCreditLimit () {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CreditAccount account = new CreditAccount(
-                    3_000,
-                    5_000,
-                    15
-            );
-        });
-    }
-
-    @Test // некорректный расчет: актуальное значение получается -3000
+    @Test
     public void payLessBalance() {
         CreditAccount account = new CreditAccount(
                 5_000,
@@ -114,7 +94,7 @@ public class CreditAccountTest {
         Assertions.assertEquals(2_000, account.getBalance());
     }
 
-    @Test // некорректный расчет: актуальное значение получается -6000
+    @Test
     public void payMoreBalance() {
         CreditAccount account = new CreditAccount(
                 5_000,
@@ -127,7 +107,7 @@ public class CreditAccountTest {
         Assertions.assertEquals(-1_000, account.getBalance());
     }
 
-    @Test // некорректный расчет: актуальное значение получается -5000
+    @Test
     public void payEqualBalance() {
         CreditAccount account = new CreditAccount(
                 5_000,
@@ -182,7 +162,7 @@ public class CreditAccountTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test // некорректный расчет: актуальное значение получается 3000
+    @Test
     public void addMoreNull() {
         CreditAccount account = new CreditAccount(
                 5_000,
@@ -223,43 +203,40 @@ public class CreditAccountTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test //некорректный расчет процент должен браться на сумму, которая не изменяется в течение года, а не на текущий баланс.
-    // Не хватает переменной для неснижаемого остатка.
+    @Test
     public void yearChangeNonDeductibleAccountBalanceMoreNull() {
         CreditAccount account = new CreditAccount(
-                5_000,
+                1_000,
                 5_000,
                 15
         );
 
-        // нет ожидания, пока нет поля для неснижаемого остатка в течение года
+        account.pay(500);
         Assertions.assertEquals(0, account.yearChange());
     }
 
-    @Test //некорректный расчет процент должен браться на сумму, которая не изменяется в течение года, а не на текущий баланс.
-    // Не хватает переменной для неснижаемого остатка.
+    @Test
     public void yearChangeNonDeductibleAccountBalanceEquallyNull() {
         CreditAccount account = new CreditAccount(
-                0,
+                1_000,
                 5_000,
                 15
         );
 
-        // нет ожидания, пока нет поля для неснижаемого остатка в течение года
+        account.pay(1_000);
         Assertions.assertEquals(0, account.yearChange());
     }
 
-    @Test //некорректный расчет процент должен браться на сумму, которая не изменяется в течение года, а не на текущий баланс.
-    // Не хватает переменной для неснижаемого остатка.
+    @Test
+
     public void yearChangeNonDeductibleAccountBalanceLessNull() {
         CreditAccount account = new CreditAccount(
-                -3_000,
+                1_000,
                 5_000,
                 15
         );
 
-        // нет ожидания, пока нет поля для неснижаемого остатка в течение года
-        Assertions.assertEquals(0, account.yearChange());
+        account.pay(4_000);
+        Assertions.assertEquals(-450, account.yearChange());
     }
-
 }
