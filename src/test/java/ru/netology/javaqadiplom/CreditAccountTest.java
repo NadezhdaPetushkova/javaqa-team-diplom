d725a00afce4bac1f04a26c64a06d764399cd7c2
Olga <o.n.p@mail.ru>
2023-06-20 14:34:01 +0300

    Add tests

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
 
-    @Test
+    // Аналогичный тест в блоке тестирования add
+    /* @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
                 0,
@@ -16,7 +17,55 @@ public class CreditAccountTest {
         account.add(3_000);
 
         Assertions.assertEquals(3_000, account.getBalance());
+    }*/
+
+    /*@Test // в случае необходимости тест для условия: при создании, баланс кредитного счёта изначально выставляется в кредитный лимит.
+    public void exceptionInitialBalanceMoreCreditLimit () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    10_000,
+                    5_000,
+                    15
+            );
+        });
     }
+
+    @Test // в случае необходимости тест для условия: при создании, баланс кредитного счёта изначально выставляется в кредитный лимит.
+    public void exceptionInitialBalanceMoreCreditLimitForOne () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    5_001,
+                    5_000,
+                    15
+            );
+        });
+    }
+
+    @Test // в случае необходимости тест для условия: при создании, баланс кредитного счёта изначально выставляется в кредитный лимит.
+    public void exceptionInitialBalanceLessCreditLimit () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    3_000,
+                    5_000,
+                    15
+            );
+        });
+    }
+
+    @Test // в случае необходимости тест для условия: при создании, баланс кредитного счёта изначально выставляется в кредитный лимит.
+    public void exceptionInitialBalanceLessCreditLimitForOne () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    3_000,
+                    3_001,
+                    15
+            );
+        });
+    }*/
 
     @Test
     public void exceptionRateBelowNull() {
@@ -30,13 +79,33 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void exceptionRateEqualNull() {
+    public void RateEqualNull() {
         CreditAccount account = new CreditAccount(
                 5_000,
                 5_000,
                 0
         );
         Assertions.assertEquals(0, account.getRate());
+    }
+
+    @Test
+    public void RateEqualOne() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                1
+        );
+        Assertions.assertEquals(1, account.getRate());
+    }
+
+    @Test
+    public void RateEqualOneHundred() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                100
+        );
+        Assertions.assertEquals(100, account.getRate());
     }
 
     @Test
@@ -51,13 +120,33 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void exceptionCreditLimitNull() {
+    public void CreditLimitEqualNull() {
         CreditAccount account = new CreditAccount(
                 5_000,
                 0,
                 15
         );
         Assertions.assertEquals(0, account.getCreditLimit());
+    }
+
+    @Test
+    public void CreditLimitEqualOne() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                1,
+                15
+        );
+        Assertions.assertEquals(1, account.getCreditLimit());
+    }
+
+    @Test
+    public void CreditLimitVeryBig() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                2_100_000_000,
+                15
+        );
+        Assertions.assertEquals(2_100_000_000, account.getCreditLimit());
     }
 
     @Test
@@ -72,13 +161,33 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void exceptionInitialBalanceNull() {
+    public void InitialBalanceEqualNull() {
         CreditAccount account = new CreditAccount(
                 0,
                 5_000,
                 15
         );
         Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void InitialBalanceEqualOne() {
+        CreditAccount account = new CreditAccount(
+                1,
+                5_000,
+                15
+        );
+        Assertions.assertEquals(1, account.getBalance());
+    }
+
+    @Test
+    public void InitialBalanceVeryBig() {
+        CreditAccount account = new CreditAccount(
+                2_100_000_000,
+                5_000,
+                15
+        );
+        Assertions.assertEquals(2_100_000_000, account.getBalance());
     }
 
     @Test
@@ -95,16 +204,16 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void payMoreBalance() {
+    public void payLessBalanceForOne() {
         CreditAccount account = new CreditAccount(
                 5_000,
                 5_000,
                 15
         );
 
-        account.pay(6_000);
+        account.pay(4_999);
 
-        Assertions.assertEquals(-1_000, account.getBalance());
+        Assertions.assertEquals(1, account.getBalance());
     }
 
     @Test
@@ -121,6 +230,59 @@ public class CreditAccountTest {
     }
 
     @Test
+    public void payMoreBalanceForOne() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(5_001);
+
+        Assertions.assertEquals(-1, account.getBalance());
+    }
+
+    @Test
+    public void payMoreBalance() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(6_500);
+
+        Assertions.assertEquals(-1_500, account.getBalance());
+    }
+
+    @Test
+    public void payLessCreditLimitForOne() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(9_999);
+
+        Assertions.assertEquals(-4999, account.getBalance());
+    }
+
+    @Test
+    public void payEqualCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(10000);
+
+        Assertions.assertEquals(-5000, account.getBalance());
+    }
+
+
+    @Test
     public void payMoreCreditLimit() {
         CreditAccount account = new CreditAccount(
                 5_000,
@@ -128,38 +290,31 @@ public class CreditAccountTest {
                 15
         );
 
-        boolean expected = false;
-        boolean actual = account.pay(11_000);
+        account.pay(20000);
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(5000, account.getBalance());
     }
 
     @Test
-    public void amountLessNull() {
+    public void payAmountLessNull() {
         CreditAccount account = new CreditAccount(
                 5_000,
                 5_000,
                 15
         );
 
-        boolean expected = false;
-        boolean actual = account.pay(-1_000);
-
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertFalse(account.pay(-1_000));
     }
 
     @Test
-    public void amountEqualNull() {
+    public void payAmountEqualNull() {
         CreditAccount account = new CreditAccount(
                 5_000,
                 5_000,
                 15
         );
 
-        boolean expected = false;
-        boolean actual = account.pay(0);
-
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertFalse(account.pay(0));
     }
 
     @Test
@@ -176,17 +331,16 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void addLessNull() {
+    public void addOne() {
         CreditAccount account = new CreditAccount(
                 5_000,
                 5_000,
                 15
         );
 
-        boolean expected = false;
-        boolean actual = account.add(-1_000);
+        account.add(1);
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(5_001, account.getBalance());
     }
 
     @Test
@@ -197,16 +351,63 @@ public class CreditAccountTest {
                 15
         );
 
-        boolean expected = false;
-        boolean actual = account.add(0);
+        account.add(0);
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(5000, account.getBalance());
     }
 
     @Test
-    public void yearChangeNonDeductibleAccountBalanceMoreNull() {
+    public void addMinusOne() {
         CreditAccount account = new CreditAccount(
-                1_000,
+                5_000,
+                5_000,
+                15
+        );
+
+        account.add(-1);
+
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void addLessNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.add(-3_000);
+
+        Assertions.assertEquals(5000, account.getBalance());
+    }
+
+    @Test
+    public void addAmountFalseLessNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        Assertions.assertFalse(account.pay(-1_000));
+    }
+
+    @Test
+    public void addAmountFalseEqualNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        Assertions.assertFalse(account.pay(0));
+    }
+
+    @Test
+    public void yearChangeBalanceMoreNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
                 5_000,
                 15
         );
@@ -216,27 +417,157 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void yearChangeNonDeductibleAccountBalanceEquallyNull() {
+    public void yearChangeBalanceEqualOne() {
         CreditAccount account = new CreditAccount(
-                1_000,
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(4_999);
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void yearChangeBalanceEqualNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(5_000);
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void yearChangeBalanceEqualMinusOne() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(5_001);
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void yearChangeMinusOneRuble() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(5_007);
+        Assertions.assertEquals(-1, account.yearChange());
+    }
+
+    @Test
+
+    public void yearChangeBalanceLessNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(9_000);
+        Assertions.assertEquals(-600, account.yearChange());
+    }
+
+    @Test
+    public void yearChangeRoundingDecimalLessFive() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                7
+        );
+
+        account.pay(9_029);
+        Assertions.assertEquals(-282, account.yearChange());
+    }
+
+    @Test
+    public void yearChangeRoundingDecimalMoreFive() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                7
+        );
+
+        account.pay(9_028);
+        Assertions.assertEquals(-281, account.yearChange());
+    }
+
+    // проверка возможного деления на ноль, если процент может быть нулем
+    @Test
+    public void yearChangeRateNull() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                0
+        );
+
+        account.pay(9_000);
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void doubleAdd() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+        account.add(4_000);
+
+        Assertions.assertEquals(12_000, account.getBalance());
+    }
+
+    @Test
+    public void doublePay() {
+        CreditAccount account = new CreditAccount(
+                5_000,
                 5_000,
                 15
         );
 
         account.pay(1_000);
-        Assertions.assertEquals(0, account.yearChange());
+        account.pay(6_000);
+
+        Assertions.assertEquals(-2_000, account.getBalance());
     }
 
     @Test
-
-    public void yearChangeNonDeductibleAccountBalanceLessNull() {
+    public void doubleAddPay() {
         CreditAccount account = new CreditAccount(
-                1_000,
+                5_000,
                 5_000,
                 15
         );
 
-        account.pay(4_000);
-        Assertions.assertEquals(-450, account.yearChange());
+        account.add(7_000);
+        account.pay(3_000);
+
+        Assertions.assertEquals(9_000, account.getBalance());
     }
+
+    @Test
+    public void doublePayAdd() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                15
+        );
+
+        account.pay(500);
+        account.add(7_000);
+
+        Assertions.assertEquals(11_500, account.getBalance());
+    }
+
 }
